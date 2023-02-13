55c8985df512adf65c8ef20e9095106e8fb316f5
Jeremias Santos Lima <jeremias.lima@ahreas.com>
2023-02-13 00:36:39 -0300

    feat-configure-lint

--- src/presentation/screens/Login/index.tsx
+++ src/presentation/screens/Login/index.tsx
@@ -4,7 +4,6 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {LoginProps} from '../../../infra/navigation/types';
 
 const LoginScreen: React.FC<LoginProps> = ({navigation, route}) => {
-  // eslint-disable-next-line react-native/no-inline-styles
   return <SafeAreaView style={{flex: 1, backgroundColor: '#f3f3f3'}} />;
 };
 
